90ca84b70685fd71050b62aa8acd4eb097b983c5
Dcf9 <147388542+Dcf9@users.noreply.github.com>
2025-11-18 14:24:49 +0100

    Preparació format entrega i correció de faltes ortogràfiques

--- Pregunta1/trobarCami.py
+++ Pregunta1/trobarCami.py
@@ -1,7 +1,7 @@
 """
 
 Practica 3 d'IA: Pregunta 1
-@authors: Pol Lobo & Damia Carreras
+@authors: Pol Lobo & Damià Carreras
 
 """
 
@@ -13,75 +13,104 @@ from tqdm import trange
 
 class TrobarCami():
     """
-    ATRIBUTES
-    ---------
-    sailorPosition : tuple
-        Posició actual del mariner a la graella
+
+    Atributes
+    -------------------------------------------
     start : tuple
         Posició d'inici
     goal : tuple
         Posició objectiu
     exercici : str
         Exercici a o b
-    map : np.array
-        Graella que representa l'entorn
-
-    METHODS
-    -------
-    initial_state() -> None
+    levelOfDrunkness : float
+        Nivell d'embriaguesa (randomness)
+    -------------------------------------------
+        
+    Methods
+    -------------------------------------------
+    initial_position() -> None
         Inicialitza l'estat segons l'exercici escollit (a o b)
     
     initiate_map() -> None
         Inicialitza la graella i el mariner
 
+    initiate_q_table() -> None
+        Inicialitza la taula Q i els diccionaris d'estat-posició i posició-estat    
+
     print_map() -> None
         Dibuixa la graella i un cercle indicant on es el mariner
+
+    print_q_table() -> None
+        Imprimeix per terminal la Q-table
+
+    epsilon_greeedy_policy(state, epsilon) -> int
+        Política d'actuació amb epsilon-greedy
+    
+    greedy_policy(state) -> int
+        Política d'actuació greedy
 
     move_sailor(new_pos) -> None
         Movem el mariner a la nova posició i actualitzem el dibuix
 
+    move_simulator(new_pos) -> None
+        Movem el mariner en el simulador d'entrenament
+
+    reward(state) -> float
+        Retorna la recompensa associada a una posició donada
+    
+    goalReached(position) -> bool
+        Comprova si s'ha arribat a la posició final
+
+    doAnEpisode(policy, epsilon=None) -> None
+        Realitza un episodi complet seguint la política donada
+    
+    train() -> None
+        Entrena l'agent amb Q-learning
+
     q_learning() -> None
-        Implementa l'algorisme de Q-learning per trobar el cami optim fins la meta
+        Implementa l'algorisme Q-learning per trobar el camí òptim fins a la posició objectiu
+    -------------------------------------------
 
     """
 
     def __init__(self, start, goal, exercici, levelOfDrunkness=0):
+
         #---------- Atributs pel mapa i el mariner ----------#
-        self.sailorPosition = start             # Posició inicial
-        self.levelOfDrunknss = levelOfDrunkness # Nivell d'embriaguesa (randomness)
-        self.n_hiting_wall = 0                  # Comptador de cops a la paret
-        self.sailorPositionSimulator = start    # Posició inicial per entrenar    
+        self.sailorPosition = start                                 # Posició inicial
+        self.levelOfDrunknss = levelOfDrunkness                     # Nivell d'embriaguesa (randomness)
+        self.n_hiting_wall = 0                                      # Comptador de cops a la paret
+        self.sailorPositionSimulator = start                        # Posició inicial per entrenar    
         self.start = start              
-        self.goal = goal                        # Posició objectiu
-        self.exercici = exercici                # Exercici a o b
+        self.goal = goal                                            # Posició objectiu
+        self.exercici = exercici                                    # Exercici a o b
 
         self.map = None
-        self.initial_position()                 # Graella inicial
+        self.initial_position()                                     # Graella inicial amb les recompenses en funció de l'exercici
 
-        self.initiate_map()                     # Dibuixem la graella
+        self.initiate_map()                                         # Dibuixem la graella
         #-----------------------------------------------------#
 
         #-------------- Atributs per Q-learning --------------#
-        self.qTable = None                  # Taula Q
-        self.posToState = {}                # Diccionari d'estats per la taula Q
-        self.stateToPos = {}                # Diccionari de posicions per la taula Q
+        self.qTable = None                                          # Taula Q
+        self.posToState = {}                                        # Diccionari d'estats per la taula Q
+        self.stateToPos = {}                                        # Diccionari de posicions per la taula Q
 
-        self.n_training_episodes = 1000     # Nombre d'episodis d'entrenament
-        self.learning_rate = 0.5            # Taxa d'aprenentatge (alpha)
+        self.n_training_episodes = 1000                             # Nombre d'episodis d'entrenament
+        self.learning_rate = 0.5                                    # Taxa d'aprenentatge (alpha)
 
-        self.n_eval_parametres = 100        # Nombre d'episodis per evaluar
+        self.n_eval_parametres = 100                                # Nombre d'episodis per avaluar
 
-        self.max_steps = 100                # Nombre maxim de passos per episodi
-        self.gamma = 0.95                   # Factor de descompte (gamma)             
+        self.max_steps = 100                                        # Nombre màxim d'iteracions per episodi
+        self.gamma = 0.95                                           # Factor de descompte (gamma)             
 
-        self.max_epsilon = 1               # Probabilitat d'exploració
-        self.min_epsilon = 0.1             # Probabilitat mínima d'exploració
-        self.decay_rate = 0.01             # Taxa de decaïment d'epsilon
+        self.max_epsilon = 1                                        # Probabilitat d'exploració
+        self.min_epsilon = 0.1                                      # Probabilitat mínima d'exploració
+        self.decay_rate = 0.01                                      # Taxa de decaiguda d'epsilon
         #-----------------------------------------------------#
 
     def initial_position(self):
         """
-        Inicialitza la posicio segons l'exercici escollit (a o b).
+        Inicialitza la posició segons l'exercici escollit (a o b).
         """
         if self.exercici == 'a':
             self.map = np.array([[-1, -1, -1, 100],
@@ -94,7 +123,7 @@ class TrobarCami():
 
     def initiate_map(self):
         """
-        Dibuixa la graella i el mariner a la posició inicial.
+        Inicialitza la graella i situa el mariner a la posició inicial.
         """
         plt.ion()  # Mode interactiu activat
 
@@ -109,9 +138,37 @@ class TrobarCami():
         )
         self.ax.add_patch(self.cercle)
 
+    def initiate_q_table(self):
+        """
+        El nostre mariner pot moure's en 4 direccions: (Columnes)
+            - (0) Amunt
+            - (1) Avall
+            - (2) Esquerra
+            - (3) Dreta
+
+        El nostre mariner té 4x3 - 1 = 11 estats possibles: (Files)
+            Cada posicio sera un estat diferent
+        """
+
+        n_files, n_cols = self.map.shape
+        comptador_estats = 0
+
+        # Inicialitzem el diccionari
+        for i in range(n_files):
+            for j in range(n_cols):
+                if self.map[i, j] is not None:
+                    self.posToState[(i, j)] = comptador_estats
+                    self.stateToPos[comptador_estats] = (i, j)
+                    comptador_estats += 1
+
+        # Inicialitzem la taula Q amb zeros
+        self.qTable = np.zeros((comptador_estats, 4))
+
+        print("Q-table initialized: \n", self.qTable)
+
     def print_map(self):
         """
-        Dibuixa la graella i un cercle indicant on es el mariner.
+        Dibuixa la graella i amb un cercle indicant la posició del mariner.
         """
         n_files, n_col = self.map.shape
         color_array = np.zeros((n_files, n_col, 3))
@@ -153,6 +210,34 @@ class TrobarCami():
         self.ax.set_xlim(-0.5, n_col-0.5)
         self.ax.set_ylim(n_files-0.5, -0.5)
 
+    def print_q_table(self):
+        print("Q-table: \n", self.qTable)
+    
+    def epsilon_greeedy_policy(self, state, epsilon):
+        """
+        Política d'actuació
+        Fixarem un epsilon, que definirà les probabilitats:
+            - epsilon: Probabilitat d'exploració (triar una acció aleatòria)
+            - (1 - epsilon): Probabilitat d'explotació (triar la millor acció segons la taula Q)
+        Això s'escollira a partir d'un nombre aleatori de [0,1]
+        """
+        _, n_actions = self.qTable.shape
+
+        random_number = np.random.uniform(0,1)
+        if random_number < epsilon:                     # Exploració
+            action = np.random.randint(n_actions)      
+        else:                                           # Explotació
+            action = np.argmax(self.qTable[state])      
+
+        return action
+
+    def greedy_policy(self, state):
+        """
+        Politica d'actualització
+        Sempre tria la millor acció segons la taula Q
+        """
+        return np.argmax(self.qTable[state])
+
     def move_sailor(self, action):
         """
         Movem el mariner a la nova posició i actualitzem el dibuix.
@@ -174,7 +259,7 @@ class TrobarCami():
         elif action == 3:   # Dreta
             new_row, new_col = row, col + 1
         else:
-            raise ValueError("Acció no vàlida")
+            raise ValueError("Acció invàlida")
 
         # Comprovem si el moviment es valid
         if not ((new_row < 0 or new_row >= n_files) or (new_col < 0 or new_col >= n_col) or (self.map[new_row, new_col] is None)):
@@ -189,65 +274,9 @@ class TrobarCami():
         self.n_hiting_wall += 1
         return False
 
-    def initiate_q_table(self):
-        """
-        El nostre mariner pot moure's en 4 direccions: (Columnes)
-            - (0) Amunt
-            - (1) Avall
-            - (2) Esquerra
-            - (3) Dreta
-
-        El nostre mariner té 4x4 - 1 = 15 estats possibles: (Files)
-            Cada posicio sera un estat diferent
-        """
-
-        n_files, n_cols = self.map.shape
-        comptador_estats = 0
-
-        # Inicialitzem el diccionari
-        for i in range(n_files):
-            for j in range(n_cols):
-                if self.map[i, j] is not None:
-                    self.posToState[(i, j)] = comptador_estats
-                    self.stateToPos[comptador_estats] = (i, j)
-                    comptador_estats += 1
-
-        # Inicialitzem la taula Q amb zeros
-        self.qTable = np.zeros((comptador_estats, 4))
-
-        print("Q-table initialized: \n", self.qTable)
-
-    def print_q_table(self):
-        pass
-
-    def epsilon_greeedy_policy(self, state, epsilon):
-        """
-        Politica d'actuacio
-        Fixarem un epsilon, que definira les probabilitats:
-            - epsilon: Probabilitat d'exploració (triar una acció aleatòria)
-            - (1 - epsilon): Probabilitat d'explotació (triar la millor acció segons la taula Q)
-        Això s'escollira a partir d'un numero aleatori de [0,1]
-        """
-        _, n_actions = self.qTable.shape
-
-        random_number = np.random.uniform(0,1)
-        if random_number < epsilon:                 # Explorar
-            action = np.random.randint(n_actions)       # Accio aleatoria
-        else:                                       # Explotar
-            action = np.argmax(self.qTable[state])      # Millor accio de la taula Q
-
-        return action
-
-    def greedy_policy(self, state):
-        """
-        Politica d'actualitzacio
-        Sempre tria la millor acció segons la taula Q
-        """
-        return np.argmax(self.qTable[state])
-
     def move_simulator(self, action):
         """
-        Comprova si es possible fer el moviment
+        Comprova si es possible fer el moviment en el simulador
         """
         # Randomness per l'embriaguesa
         if np.random.uniform(0,1) < self.levelOfDrunknss:
@@ -268,13 +297,13 @@ class TrobarCami():
         else:
             raise ValueError("Acció no vàlida")
 
-        # Comprovem si el moviment es valid
+        # Comprovem si el moviment es vàlid
         if not ((new_row < 0 or new_row >= n_files) or (new_col < 0 or new_col >= n_col) or (self.map[new_row, new_col] is None)):
             # Simulem el moviment
             self.sailorPositionSimulator = (new_row, new_col)
             return True
         
-        # Moviment no valid i ens quedem a la mateixa posicio
+        # Moviment no vàlid i ens quedem a la mateixa posició
         return False
 
     def reward(self, state):
@@ -294,8 +323,9 @@ class TrobarCami():
         Realitza un episodi complet seguint la política donada
         Policy: 
             - True: epsilon_greeedy_policy  --> Training
-            - False: greedy_policy          --> Evaluation
+            - False: greedy_policy          --> Deployment
         """
+        
         # Inicialitzem l'episodi
         if policy:
             self.sailorPositionSimulator = self.start
@@ -304,25 +334,28 @@ class TrobarCami():
         else:
             self.sailorPosition = self.start
             state = self.posToState[self.sailorPosition]
+
         step = 0
 
         # Repeat step
         for step in range(self.max_steps):
 
             if policy:
+
                 action = self.epsilon_greeedy_policy(state, epsilon)
                 
                 self.move_simulator(action)
 
-                # En el move_simulator ja actualitzem la posicio del simulador
+                # En el move_simulator ja actualitzem la posició del mariner
                 newPosition = self.sailorPositionSimulator
 
             else:
+
                 action = self.greedy_policy(state)
 
                 self.move_sailor(action)
 
-                # En el move_sailor ja actualitzem la posicio del mariner
+                # En el move_sailor ja actualitzem la posició del mariner
                 newPosition = self.sailorPosition
 
             newState = self.posToState[newPosition]
@@ -365,7 +398,7 @@ class TrobarCami():
         print("\nTraining in process...")
         self.train()
 
-        print("\nFinal Q-table: \n", self.qTable)
+        self.print_q_table()
 
         print("\nEvaluating the agent...\n")
 
@@ -381,17 +414,24 @@ class TrobarCami():
         # plt.show(block=True)
         plt.pause(2)
 
-
 if __name__ == "__main__":
     # Creem la instància de la classe
     # Amb l'estat inicial de l'exercici a) o b)
-    # Start i Goal són les posicions d'inici i final
+
+    # Posició inicial
+    start = (2, 0)    
+
+    # Posició objectiu         
+    goal = (0, 3)
     
+
     # EXERCICI 1.a)
-    # cami = TrobarCami((2,0), (0, 3), "a")
+    # cami = TrobarCami(start, goal, "a")
+
     # EXERCICI 1.b)
-    # cami = TrobarCami((2,0), (0, 3), "b")
+    # cami = TrobarCami(start, (goal, "b")
+
     # EXERCICI 1.c)
-    cami = TrobarCami((2,0), (0, 3), "b", 0.01)
+    cami = TrobarCami(start, goal, "b", 0.01)
 
     cami.q_learning()
